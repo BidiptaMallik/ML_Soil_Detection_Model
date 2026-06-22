19134e34c13635755dd490b8f921afa9a9aa6be1
Bidipta Mallik <bidiptamallik0@gmail.com>
2026-06-23 00:55:26 +0530

    Deployment Update

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ml-soil-detection-model-2-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
