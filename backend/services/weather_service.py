4fe7c653708e872d5a9d1df9ef95427adf77701c
Bidipta Mallik <bidiptamallik0@gmail.com>
2026-06-23 16:07:42 +0530

    Deployment Update

--- backend/services/weather_service.py
+++ backend/services/weather_service.py
@@ -1,31 +1,20 @@
 import requests
 from dotenv import load_dotenv
-import os 
+import os
 
 load_dotenv()
 
-
 def get_weather(city):
-
+    API_KEY = os.getenv("WEATHER_API_KEY")
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     
-
-    API_KEY =  os.getenv("WEATHER_API_KEY")
-
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric"
-
     data = requests.get(url).json()
 
-    if "list" not in data:
+    if "main" not in data:
         raise Exception("Weather API failed")
 
-    # take next 24h forecast (important upgrade)
-    next_hours = data["list"][:8]   # 3-hour steps × 8 = 24h
-
-    temp = sum([x["main"]["temp"] for x in next_hours]) / len(next_hours)
-    humidity = sum([x["main"]["humidity"] for x in next_hours]) / len(next_hours)
-
-    rainfall = sum([
-        x.get("rain", {}).get("3h", 0) for x in next_hours
-    ])
+    temp = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    rainfall = data.get("rain", {}).get("1h", 0)
 
     return temp, humidity, rainfall
